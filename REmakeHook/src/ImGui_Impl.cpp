0a802a7dd4871fff4dbef6938ba4f7dbbf2d5f3a
Eleval <tissotxavier@gmail.com>
2022-09-18 21:33:26 +0200

    Cleaned some ImGui code

--- REmakeHook/src/ImGui_Impl.cpp
+++ REmakeHook/src/ImGui_Impl.cpp
@@ -3,10 +3,10 @@
 #include "ImGui_Impl.h"
 
 #include "Game/GameData.h"
+#include "Utils/CallHook.h"
 
 #include "imgui/imgui.h"
 
-#include "Utils/CallHook.h"
 
 #include <chrono>
 
@@ -123,38 +123,6 @@ bool IsVkDown(int vk)
 	return (::GetKeyState(vk) & 0x8000) != 0;
 }
 
-/*void AddKeyEvent(ImGuiKey key, bool down, int native_keycode, int native_scancode = -1)
-{
-	ImGuiIO& io = ImGui::GetIO();
-	io.AddKeyEvent(key, down);
-	io.SetKeyEventNativeData(key, native_keycode, native_scancode); // To support legacy indexing (<1.87 user code)
-	IM_UNUSED(native_scancode);
-}*/
-
-/*void ProcessKeyEventsWorkarounds()
-{
-	// Left & right Shift keys: when both are pressed together, Windows tend to not generate the WM_KEYUP event for the first released one.
-	if (ImGui::IsKeyDown(ImGuiKey_LeftShift) && !IsVkDown(VK_LSHIFT))
-		AddKeyEvent(ImGuiKey_LeftShift, false, VK_LSHIFT);
-	if (ImGui::IsKeyDown(ImGuiKey_RightShift) && !IsVkDown(VK_RSHIFT))
-		AddKeyEvent(ImGuiKey_RightShift, false, VK_RSHIFT);
-
-	// Sometimes WM_KEYUP for Win key is not passed down to the app (e.g. for Win+V on some setups, according to GLFW).
-	if (ImGui::IsKeyDown(ImGuiKey_LeftSuper) && !IsVkDown(VK_LWIN))
-		AddKeyEvent(ImGuiKey_LeftSuper, false, VK_LWIN);
-	if (ImGui::IsKeyDown(ImGuiKey_RightSuper) && !IsVkDown(VK_RWIN))
-		AddKeyEvent(ImGuiKey_RightSuper, false, VK_RWIN);
-}*/
-
-/*void UpdateKeyModifiers()
-{
-	ImGuiIO& io = ImGui::GetIO();
-	io.AddKeyEvent(ImGuiKey_ModCtrl, IsVkDown(VK_CONTROL));
-	io.AddKeyEvent(ImGuiKey_ModShift, IsVkDown(VK_SHIFT));
-	io.AddKeyEvent(ImGuiKey_ModAlt, IsVkDown(VK_MENU));
-	io.AddKeyEvent(ImGuiKey_ModSuper, IsVkDown(VK_APPS));
-}*/
-
 void UpdateMouseData()
 {
 	ImGuiIO& io = ImGui::GetIO();
@@ -173,10 +141,6 @@ void UpdateMouseData()
 		// (Optional) Fallback to provide mouse position when focused (WM_MOUSEMOVE already provides this when hovered or captured)
 		if (!io.WantSetMousePos && !pd_.mouseTracked)
 		{
-			/*POINT pos;
-			if (::GetCursorPos(&pos) && ::ScreenToClient(pd_.hwnd, &pos))
-				io.AddMousePosEvent((float)pos.x, (float)pos.y);*/
-			
 			POINT pos;
 			if (::GetCursorPos(&pos) && ::ScreenToClient(pd_.hwnd, &pos))
 			{
@@ -191,122 +155,6 @@ void UpdateMouseData()
 		io.MouseDown[2] = IsVkDown(VK_MBUTTON);
 	}
 }
-
-// There is no distinct VK_xxx for keypad enter, instead it is VK_RETURN + KF_EXTENDED, we assign it an arbitrary value to make code more readable (VK_ codes go up to 255)
-#define IM_VK_KEYPAD_ENTER      (VK_RETURN + 256)
-
-// Map VK_xxx to ImGuiKey_xxx.
-/*ImGuiKey VirtualKeyToImGuiKey(WPARAM wParam)
-{
-	switch (wParam)
-	{
-		case VK_TAB: return ImGuiKey_Tab;
-		case VK_LEFT: return ImGuiKey_LeftArrow;
-		case VK_RIGHT: return ImGuiKey_RightArrow;
-		case VK_UP: return ImGuiKey_UpArrow;
-		case VK_DOWN: return ImGuiKey_DownArrow;
-		case VK_PRIOR: return ImGuiKey_PageUp;
-		case VK_NEXT: return ImGuiKey_PageDown;
-		case VK_HOME: return ImGuiKey_Home;
-		case VK_END: return ImGuiKey_End;
-		case VK_INSERT: return ImGuiKey_Insert;
-		case VK_DELETE: return ImGuiKey_Delete;
-		case VK_BACK: return ImGuiKey_Backspace;
-		case VK_SPACE: return ImGuiKey_Space;
-		case VK_RETURN: return ImGuiKey_Enter;
-		case VK_ESCAPE: return ImGuiKey_Escape;
-		case VK_OEM_7: return ImGuiKey_Apostrophe;
-		case VK_OEM_COMMA: return ImGuiKey_Comma;
-		case VK_OEM_MINUS: return ImGuiKey_Minus;
-		case VK_OEM_PERIOD: return ImGuiKey_Period;
-		case VK_OEM_2: return ImGuiKey_Slash;
-		case VK_OEM_1: return ImGuiKey_Semicolon;
-		case VK_OEM_PLUS: return ImGuiKey_Equal;
-		case VK_OEM_4: return ImGuiKey_LeftBracket;
-		case VK_OEM_5: return ImGuiKey_Backslash;
-		case VK_OEM_6: return ImGuiKey_RightBracket;
-		case VK_OEM_3: return ImGuiKey_GraveAccent;
-		case VK_CAPITAL: return ImGuiKey_CapsLock;
-		case VK_SCROLL: return ImGuiKey_ScrollLock;
-		case VK_NUMLOCK: return ImGuiKey_NumLock;
-		case VK_SNAPSHOT: return ImGuiKey_PrintScreen;
-		case VK_PAUSE: return ImGuiKey_Pause;
-		case VK_NUMPAD0: return ImGuiKey_Keypad0;
-		case VK_NUMPAD1: return ImGuiKey_Keypad1;
-		case VK_NUMPAD2: return ImGuiKey_Keypad2;
-		case VK_NUMPAD3: return ImGuiKey_Keypad3;
-		case VK_NUMPAD4: return ImGuiKey_Keypad4;
-		case VK_NUMPAD5: return ImGuiKey_Keypad5;
-		case VK_NUMPAD6: return ImGuiKey_Keypad6;
-		case VK_NUMPAD7: return ImGuiKey_Keypad7;
-		case VK_NUMPAD8: return ImGuiKey_Keypad8;
-		case VK_NUMPAD9: return ImGuiKey_Keypad9;
-		case VK_DECIMAL: return ImGuiKey_KeypadDecimal;
-		case VK_DIVIDE: return ImGuiKey_KeypadDivide;
-		case VK_MULTIPLY: return ImGuiKey_KeypadMultiply;
-		case VK_SUBTRACT: return ImGuiKey_KeypadSubtract;
-		case VK_ADD: return ImGuiKey_KeypadAdd;
-		case IM_VK_KEYPAD_ENTER: return ImGuiKey_KeypadEnter;
-		case VK_LSHIFT: return ImGuiKey_LeftShift;
-		case VK_LCONTROL: return ImGuiKey_LeftCtrl;
-		case VK_LMENU: return ImGuiKey_LeftAlt;
-		case VK_LWIN: return ImGuiKey_LeftSuper;
-		case VK_RSHIFT: return ImGuiKey_RightShift;
-		case VK_RCONTROL: return ImGuiKey_RightCtrl;
-		case VK_RMENU: return ImGuiKey_RightAlt;
-		case VK_RWIN: return ImGuiKey_RightSuper;
-		case VK_APPS: return ImGuiKey_Menu;
-		case '0': return ImGuiKey_0;
-		case '1': return ImGuiKey_1;
-		case '2': return ImGuiKey_2;
-		case '3': return ImGuiKey_3;
-		case '4': return ImGuiKey_4;
-		case '5': return ImGuiKey_5;
-		case '6': return ImGuiKey_6;
-		case '7': return ImGuiKey_7;
-		case '8': return ImGuiKey_8;
-		case '9': return ImGuiKey_9;
-		case 'A': return ImGuiKey_A;
-		case 'B': return ImGuiKey_B;
-		case 'C': return ImGuiKey_C;
-		case 'D': return ImGuiKey_D;
-		case 'E': return ImGuiKey_E;
-		case 'F': return ImGuiKey_F;
-		case 'G': return ImGuiKey_G;
-		case 'H': return ImGuiKey_H;
-		case 'I': return ImGuiKey_I;
-		case 'J': return ImGuiKey_J;
-		case 'K': return ImGuiKey_K;
-		case 'L': return ImGuiKey_L;
-		case 'M': return ImGuiKey_M;
-		case 'N': return ImGuiKey_N;
-		case 'O': return ImGuiKey_O;
-		case 'P': return ImGuiKey_P;
-		case 'Q': return ImGuiKey_Q;
-		case 'R': return ImGuiKey_R;
-		case 'S': return ImGuiKey_S;
-		case 'T': return ImGuiKey_T;
-		case 'U': return ImGuiKey_U;
-		case 'V': return ImGuiKey_V;
-		case 'W': return ImGuiKey_W;
-		case 'X': return ImGuiKey_X;
-		case 'Y': return ImGuiKey_Y;
-		case 'Z': return ImGuiKey_Z;
-		case VK_F1: return ImGuiKey_F1;
-		case VK_F2: return ImGuiKey_F2;
-		case VK_F3: return ImGuiKey_F3;
-		case VK_F4: return ImGuiKey_F4;
-		case VK_F5: return ImGuiKey_F5;
-		case VK_F6: return ImGuiKey_F6;
-		case VK_F7: return ImGuiKey_F7;
-		case VK_F8: return ImGuiKey_F8;
-		case VK_F9: return ImGuiKey_F9;
-		case VK_F10: return ImGuiKey_F10;
-		case VK_F11: return ImGuiKey_F11;
-		case VK_F12: return ImGuiKey_F12;
-		default: return ImGuiKey_None;
-	}
-}*/
 
 void UpdateKeyBoard()
 {
